ac9e3b46efc469da211c77c111e0a8b79f99ff2a
shadymumo <62541340+shadymumo@users.noreply.github.com>
2023-11-14 21:18:45 +0300

    Add files via upload

---
+++ DB/your_success_page.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Success Page</title>
+</head>
+<body>
+
+<h1>Operation Successful!</h1>
+
+<p>Your data has been successfully inserted/updated/deleted.</p>
+
+<!-- You can add more content or links here -->
+
+</body>
+</html>
