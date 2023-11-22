6d34c0af887160835aef6fc2c8d9566d9dca487f
BSCNRB344123 <144905282+BSCNRB344123@users.noreply.github.com>
2023-11-22 11:26:28 +0300

    Update upatecustomercontact.php

--- DB/upatecustomercontact.php
+++ DB/upatecustomercontact.php
@@ -1,59 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Update Customer Contact</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #f4f4f4;
-            margin: 0;
-            padding: 0;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-        }
 
-        form {
-            background-color: #fff;
-            padding: 20px;
-            border-radius: 8px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-            width: 300px;
-        }
-
-        label {
-            display: block;
-            margin-bottom: 8px;
-            font-weight: bold;
-        }
-
-        input {
-            width: 100%;
-            padding: 8px;
-            margin-bottom: 16px;
-            box-sizing: border-box;
-            border: 1px solid #ccc;
-            border-radius: 4px;
-        }
-
-        input[type="submit"] {
-            background-color: #4caf50;
-            color: #fff;
-            cursor: pointer;
-            padding: 10px;
-            border: none;
-            border-radius: 4px;
-        }
-
-        input[type="submit"]:hover {
-            background-color: #45a049;
-        }
-    </style>
-</head>
-<body>
 
 <?php
 // Include your database connection code here if not already included
@@ -82,15 +27,4 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 ?>
 
-    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-        <label for="customerID">Customer ID:</label>
-        <input type="text" name="customerID" required><br>
 
-        <label for="newContact">New Contact:</label>
-        <input type="text" name="newContact" required><br>
-
-        <input type="submit" value="Update Customer Contact">
-    </form>
-
-</body>
-</html>
